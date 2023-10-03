b62b8dde5cf9e839d2ac57827ea415adffa88363
keguigong <keguigong@gmail.com>
2023-10-03 18:03:07 +0800

    Set display by coding

--- app/src/main/java/com/example/ececourselab_1/MainActivity.java
+++ app/src/main/java/com/example/ececourselab_1/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.ececourselab_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView textView = new TextView(this);
+        textView.setText("Hello world!!");
+        setContentView(textView);
+//        setContentView(R.layout.activity_main);
     }
 }
