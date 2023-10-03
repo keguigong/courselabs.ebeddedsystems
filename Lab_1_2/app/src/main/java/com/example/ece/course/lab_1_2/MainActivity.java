9ca24c31e7877f2c486e2c468356841f8d89f0f1
keguigong <keguigong@gmail.com>
2023-10-03 23:42:07 +0800

    An app that can change picture by one button

--- Lab_1_2/app/src/main/java/com/example/ece/course/lab_1_2/MainActivity.java
+++ Lab_1_2/app/src/main/java/com/example/ece/course/lab_1_2/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.ece.course.lab_1_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isBoy = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnPicture = (Button) findViewById(R.id.btnPicture);
+        btnPicture.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                TextView tvName = (TextView) findViewById(R.id.tvName);
+                tvName.setText(R.string.nameBoy);
+                ImageView ivPicture = (ImageView) findViewById(R.id.ivPicture);
+                ivPicture.setImageResource(R.drawable.boy);
+                EditText etName = (EditText) findViewById(R.id.etName);
+                String name = etName.getText().toString();
+            }
+        });
     }
 }
